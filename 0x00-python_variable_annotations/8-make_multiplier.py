145abc8941f425fb15f766810f559f547637f525
MissJuly <joygitau98@gmail.com>
2023-08-04 01:18:25 +0300

    Fix errors

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-Module for cfeating a function that multiplies
+Module for creating a function that multiplies
 a float by multiplier
 """
 from typing import Callable
 
 
-def make_available(multiplier: float) -> Callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Returns:
         Callable[[float], float]: a function that multiplies a

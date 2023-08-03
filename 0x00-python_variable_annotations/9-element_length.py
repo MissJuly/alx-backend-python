7ccff59fd2ace33c86c829c3c4b4c1fb14b068dc
MissJuly <joygitau98@gmail.com>
2023-08-04 01:21:07 +0300

    Fix errors

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,7 +5,7 @@ Finding the length of each element in a list of strings.
 from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Returns:
          List[Tuple[str, int]]: A list of tuples where each

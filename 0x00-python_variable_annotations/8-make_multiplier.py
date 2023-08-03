df69f14f4731089e9e734f5a7247fca70a0e3006
MissJuly <joygitau98@gmail.com>
2023-08-04 01:14:14 +0300

    Add 9-element_length file

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module for cfeating a function that multiplies
+a float by multiplier
+"""
+from typing import Callable
+
+
+def make_available(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns:
+        Callable[[float], float]: a function that multiplies a
+        float by the multiplier
+    """
+    def multiply(n: float) -> float:
+        """
+        Returns a float
+        """
+        return n * multiplier
+    return multiply
